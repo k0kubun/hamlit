2f9616f98dcf91a06db997959a7e75b1b17a19b5
Maxim Ivanov <mivanov1@gmail.com>
2016-08-30 14:01:24 -0400

    update test name and add test for haml compatibility

--- test/hamlit/filters/plain_test.rb
+++ test/hamlit/filters/plain_test.rb
@@ -22,5 +22,17 @@ describe Hamlit::Filters do
           #{'<script>'}
       HAML
     end
+
+    it 'escapes only interpolated content if esacpe_html is set' do
+      assert_render(<<-HTML.unindent, <<-'HAML'.unindent)
+        <script>
+        &lt;script&gt;
+
+      HTML
+        :plain
+          <script>
+          #{Hamlit::Utils.escape_html '<script>'}
+      HAML
+    end
   end
 end
